fa5e2bf925f8ce030933167f96e181473918c3a9
Phuriwat18 <105902493+Phuriwat18@users.noreply.github.com>
2022-11-10 13:20:51 +0700

    commit

--- app/Models/Contents.php
+++ app/Models/Contents.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Contents extends Model
 {
     use HasFactory;
-    protected $teble = 'contents';
+    protected $table = 'contents';
     protected $fillable = ['name','detail','image'];
 }
